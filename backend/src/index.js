dcc0997b998831386c8edca5c42acab50c84ba9d
Jonskih <smolanderjoni@gmail.com>
2021-07-02 02:03:46 +0300

    minor changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-const appId = process.env.APPID || '';
+const appId = process.env.APPID || '249549d4859e4b5e6fb3a0a65198d6b5';
 const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
 const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
 
